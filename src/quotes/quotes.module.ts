c99e7694728c9298f39b462d9903dbfaf8a58031
“oavidor2” <ortal.avidor@jokr.it>
2024-12-21 01:50:02 +0200

    removed todo

--- src/quotes/quotes.module.ts
+++ src/quotes/quotes.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QuotesController } from './quotes.controller';
 import { QuotesService } from './quotes.service';
-import { Quote, QuoteSchema } from './quotes.schema'; // todo-ortal move db to its own module
+import { Quote, QuoteSchema } from './quotes.schema';
 
 @Module({
   imports: [
